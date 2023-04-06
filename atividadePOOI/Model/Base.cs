7b7665f3859bcc9f31f59a4a4b4db9fc92a6c669
MAVSxxx <toninho_bp10@hotmail.com>
2023-04-05 21:26:26 -0300

    Finalização do atividade.

--- atividadePOOI/Model/Base.cs
+++ atividadePOOI/Model/Base.cs
@@ -10,10 +10,81 @@ namespace atividadePOOI.Model
     {
         private string _produtos;
         private int _quantidade;
+        private int _codigo;
         private string _vendedor;
         private decimal _valor;
+        private decimal _venda;
+        private static decimal _vendaTotal;
+        private decimal _saldo;
+        private static decimal _saldoTotal;
 
+        public Base(string vendedor, Cliente titular, decimal saldo = 0, decimal saldoTotal = 0)
+        {
+            _vendedor = vendedor;
+            Titular = titular;
+            Saldo = saldo;
+            SaldoTotal = saldoTotal;
+        }
 
+        public Base(string vendedor, Cliente titular, string produtos, int quantidade, int codigo, decimal valor, decimal venda) {
+            _produtos = produtos;
+            _quantidade = quantidade;
+            _codigo = codigo;
+            _vendedor = vendedor;
+            _valor = valor;
+            _venda = quantidade - venda;
+            Titular = titular;
+            _vendaTotal += _venda;  
+            _saldo = valor * venda;
+            _saldoTotal += _saldo;
+        }
+
+        public string Produtos {
+            get => _produtos;
+            set => _produtos = value;
+        }
+        public int Quantidade
+        {
+            get => _quantidade;
+            set => _quantidade = value;
+        }
+        public int Codigo
+        {
+            get => _codigo;
+            set => _codigo = value;
+        }
+        public string Vendedor
+        {
+            get => _vendedor;
+            set => _vendedor = value;
+        }
+        public decimal Valor
+        {
+            get => _valor;
+            set => _valor = value;
+        }
+        public decimal Venda
+        {
+            get => _venda;
+            set => _venda = value;
+        }
+        public decimal VendaTotal
+        {
+            get => _vendaTotal;
+            set => _vendaTotal = value;
+        }
+        public decimal Saldo
+        {
+            get => _saldo;
+            set => _saldo = value;
+        }
+        public decimal SaldoTotal
+        {
+            get => _saldoTotal;
+            set => _saldoTotal = value;
+        }
+
+        public Cliente Titular { get; set;}
 
     }
 }
--- atividadePOOI/Program.cs
+++ atividadePOOI/Program.cs
@@ -1,3 +1,9 @@
 ﻿
 using atividadePOOI.Model;
 
+Cliente cliente1 = new Cliente("Beija Flor", "202-2", "10-180-210");
+Base base1 = new Base("Roger", cliente1, "toalha", 15, 0101, 25.60m, 3);
+Base base2 = new Base("Roger", cliente1, "cortina", 20, 0102, 120.80m, 2);
+
+Console.WriteLine($"Vendedor: {base1.Vendedor}; produtos: {base1.Produtos}; Empresa: {cliente1.NomeEmp}; quantidade: {base1.Quantidade}, código: {base1.Codigo}, valor: {base1.Valor}, vendidos: {base1.Venda}, saldo:{base1.Saldo}");
+Console.WriteLine($"Produto: {base2.Produtos}, Vendas: {base2.Venda}, valor: {base2.Valor}, valor total de venda; {base1.SaldoTotal}");
